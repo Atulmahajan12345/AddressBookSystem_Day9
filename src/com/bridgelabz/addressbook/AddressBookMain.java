2bbfd6dc38588733c53c9e0514c29d83a4b6aa32
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-09-01 14:17:23 +0530

    Added:Ability to create contacts in AddressBook with first and last names,adress,city,state,zip,phone number and email

---
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Address Book");
+		AddressBookService contact = new AddressBookService();
+		contact.addContact();		
+		AddressBookService contact2 = new AddressBookService();
+		contact2.addContact();		
+		contact.diaplayContacts();
+		contact2.diaplayContacts();
+	}
+
+}
+
+
